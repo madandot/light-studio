cfcc5449c680a249ed602df0a85301f19ca95ffa
madandot <reddymadan0512@gmail.com>
2023-11-30 21:20:02 +0530

    initial commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,66 +1,90 @@
 "use client";
 import React from "react";
-import toast, { Toaster } from "react-hot-toast";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 const ContactPage = () => {
+   // Validation schema using Yup
+   const validationSchema = Yup.object().shape({
+      fullname: Yup.string().required("Full name is required"),
+      email: Yup.string().email("Invalid email").required("Email is required"),
+      subject: Yup.string().required("Subject is required"),
+      message: Yup.string().required("Message is required"),
+   });
+
+   const handleSubmit = (values, { setSubmitting }) => {
+      // Perform form submission logic here
+      console.log("Form values:", values);
+      // You can make API calls or other actions with the form data
+      setSubmitting(false);
+   };
+
    return (
       <div className=' md:h-screen md:mx-0 mx-8'>
          <div className='max-w-lg mx-auto'>
-            <form className='rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white text-black '>
-               <h1 className='text-2xl font-bold '>Send a message</h1>
-               <label htmlFor='fullname' className=' font-light mt-8 '>
-                  Full name<span className='text-red-500 dark:text-gray-50'>*</span>
-               </label>
-               <input
-                  type='text'
-                  name='fullname'
-                  className='bg-transparent  rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
-               />
-               <label htmlFor='email' className=' font-light mt-4 '>
-                  E-mail<span className='text-red-500'>*</span>
-               </label>
-               <input
-                  type='email'
-                  name='email'
-                  className='bg-transparent rounded  border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
-               />
-               <label htmlFor='subject' className=' font-light mt-4 '>
-                  Subject<span className='text-red-500'>*</span>
-               </label>
-               <input
-                  type='text'
-                  name='subject'
-                  className='bg-transparent rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
-               />
-               <label htmlFor='message' className=' font-light mt-4 '>
-                  Message<span className='text-red-500'>*</span>
-               </label>
-               <textarea
-                  name='message'
-                  className='bg-transparent border-2 border-[#767676] rounded py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
-               ></textarea>
-               <div className='flex flex-row items-center justify-start'>
-                  <button
-                     onClick={() => toast.success(`Your Messaged Successfully!`)}
-                     className='px-10 mt-8 py-2 bg-[#000] text-gray-50 font-light rounded-md text-lg flex flex-row items-center'
-                  >
-                     Send
-                     <svg
-                        width='24'
-                        height='24'
-                        viewBox='0 0 24 24'
-                        className='text-cyan-500 ml-2'
-                        fill='currentColor'
-                        xmlns='http://www.w3.org/2000/svg'
-                     >
-                        <path
-                           d='M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z'
-                           fill='currentColor'
-                        />
-                     </svg>
-                  </button>
-               </div>
-            </form>
-            <Toaster />
+            <Formik
+               initialValues={{
+                  fullname: "",
+                  email: "",
+                  subject: "",
+                  message: "",
+               }}
+               validationSchema={validationSchema}
+               onSubmit={handleSubmit}
+            >
+               {({ isSubmitting }) => (
+                  <Form className='rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white text-black'>
+                     <h1 className='text-2xl font-bold'>Send a message</h1>
+                     <label htmlFor='fullname' className='font-light mt-8'>
+                        Full name<span className='text-red-500 dark:text-gray-50'>*</span>
+                     </label>
+                     <Field
+                        type='text'
+                        name='fullname'
+                        className='bg-transparent rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
+                     />
+                     <ErrorMessage name='fullname' component='div' className='text-red-500' />
+
+                     {/* Other fields and their respective error messages */}
+                     <label htmlFor='fullname' className='font-light mt-8'>
+                        E-mail<span className='text-red-500 '>*</span>
+                     </label>
+                     <Field
+                        type='email'
+                        name='email'
+                        className='bg-transparent rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
+                     />
+                     <ErrorMessage name='email' component='div' className='text-red-500' />
+                     <label htmlFor='fullname' className='font-light mt-8'>
+                        Message<span className='text-red-500 '>*</span>
+                     </label>
+                     <Field
+                        type='text'
+                        name='message'
+                        className='bg-transparent rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
+                     />
+                     <ErrorMessage name='message' component='div' className='text-red-500' />
+                     {/* <label htmlFor='fullname' className='font-light mt-8'>
+                        Message<span className='text-red-500 '>*</span>
+                     </label>
+                     <Field
+                        type='text'
+                        name='Message'
+                        className='bg-transparent rounded border-2 border-[#767676] py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500'
+                     />
+                     <ErrorMessage name='fullname' component='div' className='text-red-500' /> */}
+                     <div className='flex flex-row items-center justify-start'>
+                        <button
+                           type='submit'
+                           disabled={isSubmitting}
+                           className='px-10 mt-8 py-2 bg-[#000] text-gray-50 font-light rounded-md text-lg flex flex-row items-center'
+                        >
+                           {isSubmitting ? "Sending..." : "Send"}
+                        </button>
+                     </div>
+                  </Form>
+               )}
+            </Formik>
          </div>
       </div>
    );
